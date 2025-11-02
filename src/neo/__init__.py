ab7e00ec7f5c09972dceed4ca5b105428adf0fd0
Yashab Alam <77148571+yashab-cyber@users.noreply.github.com>
2025-11-02 07:28:08 +0000

    ju

--- src/neo/__init__.py
+++
@@ -1,3 +0,0 @@
-"""NEO package root."""
-__all__ = ["create_app"]
-from .api.app import create_app  # noqa: E402
--- src/neo/cognitive/__init__.py
+++
@@ -1,3 +0,0 @@
-from .loader import cognitive_settings, get_cognitive_settings
-
-__all__ = ["cognitive_settings", "get_cognitive_settings"]
